d4db419f1c96a60a75271af2d5ff83dd107b9507
amaqanda <acedesigns123@gmail.com>
2025-09-01 00:22:27 +0200

    fix(firebase): remove firebase creds

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,20 +11,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, Firestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    // apiKey: "your-api-key",
-    // authDomain: "your-project-id.firebaseapp.com",
-    // projectId: "your-project-id",
-    // storageBucket: "your-project-id.appspot.com",
-    // messagingSenderId: "123456789",
-    // appId: "your-app-id",
-
-    apiKey: "AIzaSyCNjy-oRsgxQG3aixyWIKjl56rofH0-V9k",
-    authDomain: "kyc-web-app.firebaseapp.com",
-    projectId: "kyc-web-app",
-    storageBucket: "kyc-web-app.firebasestorage.app",
-    messagingSenderId: "640125528807",
-    appId: "1:640125528807:web:060bdac88cfd09d3ab5042",
-    measurementId: "G-PS14FPPG1M"
+    apiKey: "your-api-key",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "your-app-id",
 }
 
 
